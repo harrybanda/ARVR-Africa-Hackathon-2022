3592dc186514f43b815ccf295a6986c516290716
Harry Banda <harry.banda@mudaall.com>
2022-11-28 16:26:14 +0200

    Added player movement

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ const TouchGestures = require("TouchGestures");
 const Materials = require("Materials");
 const Time = require("Time");
 const Textures = require("Textures");
+const Reactive = require("Reactive");
 
 (async function () {
   const [
@@ -162,4 +163,131 @@ const Textures = require("Textures");
     }
     return coords;
   }
+
+  function addCommand(move) {
+    if (currentState === states.start) {
+      if (blocksUsed < numOfBlocks) {
+        let block = blocks[blocksUsed];
+        blocksUsed++;
+        nextBlockSlot -= blockSlotInc;
+        block.transform.y = nextBlockSlot;
+
+        // Set the block material
+        for (let i = 0; i < blockMats.length; i++) {
+          if (blockMats[i].name === move + "_block_mat") {
+            block.material = blockMats[i];
+          }
+        }
+
+        block.hidden = false;
+        commands.push({ command: move, block: block });
+        clickSound.setPlaying(true);
+        clickSound.reset();
+      }
+    }
+  }
+
+  /*------------- Initialize current level -------------*/
+
+  function initLevel() {
+    playerDir = levels[currentLevel].facing;
+
+    // Set the player's initial position
+    bunny.transform.x = pathCoordinates[0][0];
+    bunny.transform.z = pathCoordinates[0][1];
+    bunny.transform.y = playerInitY;
+
+    // set carrot position
+    let goalX = pathCoordinates[pathCoordinates.length - 1][0];
+    let goalZ = pathCoordinates[pathCoordinates.length - 1][1];
+    carrot.transform.x = goalX;
+    carrot.transform.z = goalZ;
+    carrot.transform.y = 0.03;
+    carrot.hidden = false;
+
+    // Set the player's initial direction
+    if (playerDir === "east") {
+      bunny.transform.rotationY = 0;
+    } else if (playerDir === "north") {
+      bunny.transform.rotationY = degreesToRadians(90);
+    } else if (playerDir === "west") {
+      bunny.transform.rotationY = degreesToRadians(180);
+    } else if (playerDir === "south") {
+      bunny.transform.rotationY = degreesToRadians(270);
+    }
+
+    // Add the path platforms
+    for (let i = 0; i < pathCoordinates.length; i++) {
+      let path = pathCoordinates[i];
+      let x = path[0];
+      let z = path[1];
+      let platform = platforms[i];
+      platform.transform.x = x;
+      platform.transform.z = z;
+      platform.hidden = false;
+    }
+  }
+
+  initLevel();
+
+  /*------------- Reset current level -------------*/
+
+  function resetLevel() {
+    currentState = states.start;
+    playerDir = levels[currentLevel].facing;
+    commands = [];
+    blocksUsed = 0;
+    nextBlockSlot = initBlockSlot;
+
+    bunny.hidden = false;
+
+    setTexture("btn_play");
+    Time.clearInterval(exeIntervalID);
+
+    for (let i = 0; i < numOfBlocks; i++) {
+      let block = blocks[i];
+      block.transform.y = blockInitY;
+      block.hidden = true;
+    }
+
+    initLevel();
+  }
+
+  /*------------- Go to next level -------------*/
+
+  function nextLevel(state) {
+    if (state === "next" && currentLevel < levels.length - 1) {
+      currentLevel++;
+    } else {
+      currentLevel = 0;
+    }
+
+    allCoordinates = createAllCoordinates();
+    pathCoordinates = createPathCoordinates();
+    dangerCoordinates = createDangerCoordinates();
+
+    for (let i = 0; i < numOfPlatforms; i++) {
+      let platform = platforms[i];
+      platform.hidden = true;
+    }
+
+    resetLevel();
+  }
+
+  /*------------- Utils -------------*/
+
+  function degreesToRadians(degrees) {
+    let pi = Math.PI;
+    return degrees * (pi / 180);
+  }
+
+  function setTexture(texture_name) {
+    for (let i = 0; i < buttonTextures.length; i++) {
+      if (buttonTextures[i].name === texture_name) {
+        let signal = buttonTextures[i].signal;
+        buttonMats[3].setTextureSlot("DIFFUSE", signal);
+      }
+    }
+  }
+  
 })();
